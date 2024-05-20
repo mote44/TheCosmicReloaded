017bf2208d7f09ca65f4140c00ae5bbf9e38a201
mote44 <137623605+mote44@users.noreply.github.com>
2024-05-20 19:38:49 +0200

    laserDoor3

---
+++ My project/Assets/CosmicTreasure_Root/Scripts/Escenario/DoorsScript/LaserDoorSwitch.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserDoorSwitch : MonoBehaviour
+{
+
+    Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+
+            Debug.Log("LASERSWITCH");
+            anim.Play("LaserDoorSwitchAnim");
+            GameManager.Instance.LaserButtonClick();
+        }
+    }
+}
