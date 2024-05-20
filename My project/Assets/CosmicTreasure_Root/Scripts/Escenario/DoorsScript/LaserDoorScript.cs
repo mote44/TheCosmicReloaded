689e506bedf942490d1de91ac55c1fb4b6fff3b8
PierreMerzon <153851795+PierreMerzon@users.noreply.github.com>
2024-05-20 20:31:54 +0200

    Revert "grt"
    This reverts commit 463dab8a20f7a3920a27a0432d2187c259cd219c.

--- My project/Assets/CosmicTreasure_Root/Scripts/Escenario/DoorsScript/LaserDoorScript.cs
+++ My project/Assets/CosmicTreasure_Root/Scripts/Escenario/DoorsScript/LaserDoorScript.cs
@@ -1,21 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Rendering.Universal;
 
 public class LaserDoorScript : MonoBehaviour
 {
     SpriteRenderer sprite;
     Collider2D col;
-    [SerializeField] Sprite[] spriteList;
-    Light2D laserLight;
+    [SerializeField] Sprite[] spriteList; 
     
     // Start is called before the first frame update
     void Start()
     {
        sprite = GetComponent<SpriteRenderer>();
         col = GetComponent<Collider2D>();
-        laserLight = GetComponentInChildren<Light2D>();
     }
 
     // Update is called once per frame
@@ -24,20 +21,16 @@ public class LaserDoorScript : MonoBehaviour
         if (GameManager.Instance.toOpenLaserDoor == 0)
         {
             sprite.sprite = spriteList[0];
-            laserLight.intensity = 2;
-
         }
 
         if (GameManager.Instance.toOpenLaserDoor == 1)
         {
             sprite.sprite = spriteList[1];
-            laserLight.intensity = 1;
         }
         if (GameManager.Instance.toOpenLaserDoor == 2)
         {
             sprite.sprite = spriteList[2];
             col.isTrigger = true;
-            laserLight.color = new Color(0, 1, 0, 1);
             
         }
     }
