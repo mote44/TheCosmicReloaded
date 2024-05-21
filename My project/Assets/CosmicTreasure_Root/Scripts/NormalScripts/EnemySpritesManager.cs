780088eb60a6495e13473748662346781b45ce91
mote44 <137623605+mote44@users.noreply.github.com>
2024-05-21 18:14:08 +0200

    script mov enemy

--- My project/Assets/CosmicTreasure_Root/Scripts/NormalScripts/EnemySpritesManager.cs
+++ My project/Assets/CosmicTreasure_Root/Scripts/NormalScripts/EnemySpritesManager.cs
@@ -13,6 +13,7 @@ public class EnemySpritesManager : MonoBehaviour
     float fatherRotZ;
     private SpriteRenderer spriteRenderer;
     Rigidbody2D rb;
+    
 
     // Start is called before the first frame update
     void Start()
@@ -27,9 +28,11 @@ public class EnemySpritesManager : MonoBehaviour
     {
         direction = rb.velocity;
         Debug.Log("direction " + direction);
-        GetSpriteDirection();
+        
         father = GetComponentInParent<Transform>();
         fatherRotZ = father.localRotation.z;
+        Debug.Log("FATHERZ " +fatherRotZ);
+        GetSpriteDirection();
         gameObject.GetComponent<Transform>().rotation = Quaternion.Euler(0,0,-fatherRotZ);
         HandleSpriteFlip();
     }
@@ -51,48 +54,60 @@ public class EnemySpritesManager : MonoBehaviour
     {
 
 
-        if (direction.y > 0) //North
+        if((fatherRotZ > -90 /360)|| (fatherRotZ < 90 / 360))//if (direction.y > 0) //North
         {
-            if (Mathf.Abs(direction.x) > 0) //east or west
+            anim.SetFloat("MoveY", 1);
+            if ((fatherRotZ > 0.1f / 360) && (fatherRotZ < 180f / 360)) //east or west
             {
                 Debug.Log("Entra animacion");
-                anim.SetBool("Mov_NE", true);
-                anim.SetBool("Mov_N", false);
-                anim.SetBool("Mov_SE", false);
-                anim.SetBool("Mov_S", false);
-                anim.SetBool("Mov_E", false);
+                //anim.SetBool("Mov_NE", true);
+                //anim.SetBool("Mov_N", false);
+                //anim.SetBool("Mov_SE", false);
+                //anim.SetBool("Mov_S", false);
+                //anim.SetBool("Mov_E", false);
+                anim.SetFloat("MoveX", 1);
+                
 
             }
-            else //neutral X
+            else if ((fatherRotZ < 0.1f / 360) && (fatherRotZ < -180f / 360))//neutral X
             {
-                Debug.Log("Entra animacion neutral");
-                anim.SetBool("Mov_NE", false);
-                anim.SetBool("Mov_N", true);
-                anim.SetBool("Mov_SE", false);
-                anim.SetBool("Mov_S", false);
-                anim.SetBool("Mov_E", false);
+                Debug.Log("Entra animacion neutral"); //Lo contrario de east es west
+                //anim.SetBool("Mov_NE", false);
+                //anim.SetBool("Mov_N", true);
+                //anim.SetBool("Mov_SE", false);
+                //anim.SetBool("Mov_S", false);
+                //anim.SetBool("Mov_E", false);
+                anim.SetFloat("MoveX", -1);
+                
+
             }
         }
-        else if (direction.y < 0) //South
+        else if ((fatherRotZ > 90 / 360) || (fatherRotZ < -90 / 360)) //South
         {
-            if (Mathf.Abs(direction.x) > 0) //east or west
+            anim.SetFloat("MoveY", -1);
+            if ((fatherRotZ > 0.1f / 360) && (fatherRotZ < 180f / 360)) //east or west
             {
-                anim.SetBool("Mov_NE", false);
-                anim.SetBool("Mov_N", false);
-                anim.SetBool("Mov_SE", true);
-                anim.SetBool("Mov_S", false);
-                anim.SetBool("Mov_E", false);
+                //anim.SetBool("Mov_NE", false);
+                //anim.SetBool("Mov_N", false);
+                //anim.SetBool("Mov_SE", true);
+                //anim.SetBool("Mov_S", false);
+                //anim.SetBool("Mov_E", false);
+                anim.SetFloat("MoveX", 1);
+                
+
             }
-            else
+            else if ((fatherRotZ < 0.1f / 360) && (fatherRotZ < -180f / 360))
             {
-                anim.SetBool("Mov_NE", false);
-                anim.SetBool("Mov_N", false);
-                anim.SetBool("Mov_SE", false);
-                anim.SetBool("Mov_S", true);
-                anim.SetBool("Mov_E", false);
+                //anim.SetBool("Mov_NE", false);
+                //anim.SetBool("Mov_N", false);
+                //anim.SetBool("Mov_SE", false);
+                //anim.SetBool("Mov_S", true);
+                //anim.SetBool("Mov_E", false);
+                anim.SetFloat("MoveX", -1);
+                
             }
         }
-        else //neutral
+        /*else //neutral
         {
             if (Mathf.Abs(direction.x) > 0) //east or west
             {
@@ -102,7 +117,7 @@ public class EnemySpritesManager : MonoBehaviour
                 anim.SetBool("Mov_S", false);
                 anim.SetBool("Mov_E", true);
             }
-        }
+        }*/
 
     }
 }
