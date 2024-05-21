90351e03025cbaef947ffb5a4b45cad5eddc4248
mote44 <137623605+mote44@users.noreply.github.com>
2024-05-21 11:27:05 +0200

    roof red

--- My project/Assets/CosmicTreasure_Root/Scripts/Escenario/RoofTrans.cs
+++ My project/Assets/CosmicTreasure_Root/Scripts/Escenario/RoofTrans.cs
@@ -6,6 +6,7 @@ public class RoofTrans : MonoBehaviour
 {
     private SpriteRenderer sprite;
     [SerializeField] private float transparency;
+    [SerializeField] int keyNeeded;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,7 +16,7 @@ public class RoofTrans : MonoBehaviour
     // Update is called once per frame
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player")) { sprite.color = new Color(1, 1, 1, transparency); }
+        if (collision.CompareTag("Player") && GameManager.Instance.keyChain[keyNeeded]==true) { sprite.color = new Color(1, 1, 1, transparency); }
        
     }
 
