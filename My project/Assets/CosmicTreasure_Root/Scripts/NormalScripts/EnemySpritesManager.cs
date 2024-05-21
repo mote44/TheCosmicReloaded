537c137a24cd14a20ae00c2487570a7da9f1134f
mote44 <137623605+mote44@users.noreply.github.com>
2024-05-21 21:09:39 +0200

    script EnemySprites

--- My project/Assets/CosmicTreasure_Root/Scripts/NormalScripts/EnemySpritesManager.cs
+++ My project/Assets/CosmicTreasure_Root/Scripts/NormalScripts/EnemySpritesManager.cs
@@ -31,7 +31,7 @@ public class EnemySpritesManager : MonoBehaviour
         
         father = GetComponentInParent<Transform>();
         fatherRotZ = father.localRotation.z;
-        Debug.Log("FATHERZ " +fatherRotZ);
+        //Debug.Log("FATHERZ " +fatherRotZ);
         GetSpriteDirection();
         gameObject.GetComponent<Transform>().rotation = Quaternion.Euler(0,0,-fatherRotZ);
         HandleSpriteFlip();
@@ -54,62 +54,107 @@ public class EnemySpritesManager : MonoBehaviour
     {
 
 
-        if((fatherRotZ > -90f / 180 && fatherRotZ < 0f) || (fatherRotZ < 90f / 180 && fatherRotZ >0f))//if (direction.y > 0) //North         //-0.5 / 0.5
+        if(fatherRotZ >= -45f / 180 && fatherRotZ <= 45f / 180 )//if (direction.y > 0) //North   //-0.25 / 0.25      
         {
             anim.SetFloat("Move_Y", 1f);
-            
-            if ((fatherRotZ > 0.1f / 180) && (fatherRotZ < 180f / 180)) //east or west          //0.00027 /.5
+
+            if ((fatherRotZ > 22f / 180) && (fatherRotZ < 45f / 180)) //east or west         
             {
-                Debug.Log("Entra animacion");
-                //anim.SetBool("Mov_NE", true);
-                //anim.SetBool("Mov_N", false);
-                //anim.SetBool("Mov_SE", false);
-                //anim.SetBool("Mov_S", false);
-                //anim.SetBool("Mov_E", false);
+                Debug.Log("NE");
+                
                 anim.SetFloat("MoveX", 1);
-                
 
             }
-            else if ((fatherRotZ < -0.1f / 180) && (fatherRotZ > -180f / 180))//neutral X
+            else if ((fatherRotZ < -22f / 180) && (fatherRotZ > -45f / 180))//neutral X
             {
-                Debug.Log("Entra animacion neutral"); //Lo contrario de east es west
-                //anim.SetBool("Mov_NE", false);
-                //anim.SetBool("Mov_N", true);
-                //anim.SetBool("Mov_SE", false);
-                //anim.SetBool("Mov_S", false);
-                //anim.SetBool("Mov_E", false);
+                Debug.Log("NW"); //Lo contrario de east es west
+               
                 anim.SetFloat("MoveX", -1);
-                
 
             }
+            else if ((fatherRotZ >= -22f / 180) && (fatherRotZ <=22 / 180)) { anim.SetFloat("MoveX", 0);  Debug.Log("N"); }
         }
-        else if ((fatherRotZ > 90 / 180 && fatherRotZ < 180/180) || (fatherRotZ < -90 / 180 && fatherRotZ > -180/180)) //South
+        
+
+
+        
+        
+        else if ((fatherRotZ >= 135 / 180 && fatherRotZ < 180/180) || (fatherRotZ <= -135 / 180 && fatherRotZ > -180/180)) //South  //0.75 / 1 || -1 /-0.75
         {
-            Debug.Log("SUR");
+            
             anim.SetFloat("Move_Y", -1f);
            
-            if ((fatherRotZ > 0.1f / 180) && (fatherRotZ < 180f / 180)) //east or west
+            if ((fatherRotZ > 135f / 180) && (fatherRotZ < 157 / 180)) //east or west
             {
-                //anim.SetBool("Mov_NE", false);
-                //anim.SetBool("Mov_N", false);
-                //anim.SetBool("Mov_SE", true);
-                //anim.SetBool("Mov_S", false);
-                //anim.SetBool("Mov_E", false);
+                Debug.Log("SE");
                 anim.SetFloat("MoveX", 1);
                 
 
             }
-            else if ((fatherRotZ < 0.1f / 180) && (fatherRotZ < -180f / 180))
+            else if ((fatherRotZ < -135f / 180) && (fatherRotZ > -157f / 180))
             {
-                //anim.SetBool("Mov_NE", false);
-                //anim.SetBool("Mov_N", false);
-                //anim.SetBool("Mov_SE", false);
-                //anim.SetBool("Mov_S", true);
-                //anim.SetBool("Mov_E", false);
+
+                Debug.Log("SW");
                 anim.SetFloat("MoveX", -1);
                 
             }
+            else if ((fatherRotZ <= -157f / 180) && (fatherRotZ >= -180f / 180) || (fatherRotZ > 157f / 180) && (fatherRotZ <= 180f / 180)) { anim.SetFloat("MoveX", 0); }
         }
+
+
+
+
+
+
+
+
+        else if (fatherRotZ > 45f / 180 && fatherRotZ< 135f / 180)//if (direction.y > 0) //EAST         //0.25 / 0.75
+        {
+            anim.SetFloat("MoveX", 1f);
+
+            if ((fatherRotZ > 45f / 180) && (fatherRotZ <= 67f / 180)) //north or south         
+            {
+                //Debug.Log("Entra animacion");
+                Debug.Log("EN");
+                anim.SetFloat("Move_Y", 1);
+
+
+            }
+            else if ((fatherRotZ < 135f / 180) && (fatherRotZ >= 112f / 180))//neutral X
+            {
+                //Debug.Log("Entra animacion neutral"); //Lo contrario de east es west
+                Debug.Log("ES");
+                anim.SetFloat("Move_Y", -1);
+
+
+            }
+            else if ((fatherRotZ < 135f / 180) && (fatherRotZ > 45f / 180)) { anim.SetFloat("Move_Y", 0); }
+        }
+
+
+        else if (fatherRotZ > -135f / 180 && fatherRotZ< -45f / 180)//if (direction.y > 0) //WEST         //-0.75 / -0.25
+        {
+            anim.SetFloat("MoveX", -1f);
+
+            if ((fatherRotZ < -45f / 180) && (fatherRotZ >= -67f / 180)) //north or south          
+            {
+                //Debug.Log("Entra animacion");
+                Debug.Log("WN");
+                anim.SetFloat("Move_Y", 1);
+
+
+            }
+            else if ((fatherRotZ > -135f / 180) && (fatherRotZ <= -112f / 180))//neutral X
+            {
+                //Debug.Log("Entra animacion neutral"); //Lo contrario de east es west
+                Debug.Log("WS");
+                anim.SetFloat("Move_Y", -1);
+
+
+            }
+            else if ((fatherRotZ > -135f / 180) && (fatherRotZ <= -45f / 180)) { anim.SetFloat("Move_Y", 0); }
+        }
+
         /*else //neutral
         {
             if (Mathf.Abs(direction.x) > 0) //east or west
