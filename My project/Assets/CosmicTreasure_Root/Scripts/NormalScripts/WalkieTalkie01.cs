1eca4b73135ef9f40c65986b2b8a5c2678d6105d
mote44 <137623605+mote44@users.noreply.github.com>
2024-05-22 16:54:02 +0200

    walkie01

--- My project/Assets/CosmicTreasure_Root/Scripts/NormalScripts/WalkieTalkie01.cs
+++ My project/Assets/CosmicTreasure_Root/Scripts/NormalScripts/WalkieTalkie01.cs
@@ -1,14 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class WalkieTalkie01 : MonoBehaviour
 {
     Animator anim;
+    SpriteRenderer sprite;
+    Light2D walkieLight;
+    private int audioDuration;
     // Start is called before the first frame update
     void Start()
     {
         anim = GetComponent<Animator>();
+        sprite = GetComponent<SpriteRenderer>();
+        walkieLight = GetComponentInChildren<Light2D>();
+        sprite.color = new Color(1, 1, 1, 0);
+        walkieLight.intensity = 0;
         StartCoroutine("WalkieRutine");
     }
 
@@ -21,7 +29,11 @@ public class WalkieTalkie01 : MonoBehaviour
     IEnumerator WalkieRutine()
     {
         AudioManager.instance.PlaySFX(42);
+        sprite.color = new Color(1, 1, 1, 1);
         anim.Play("WalkieAnim");
+        walkieLight.intensity = 1;
         yield return new WaitForSeconds(10);
+        sprite.color = new Color(1, 1, 1, 0);
+        walkieLight.intensity = 0;
     }
 }
