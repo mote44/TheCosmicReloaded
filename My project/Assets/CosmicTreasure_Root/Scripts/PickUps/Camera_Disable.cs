2279b0e90ca2bc17bf65fc2f3eeed7b090b82a70
PierreMerzon <153851795+PierreMerzon@users.noreply.github.com>
2024-05-23 01:09:54 +0200

    thr

--- My project/Assets/CosmicTreasure_Root/Scripts/PickUps/Camera_Disable.cs
+++ My project/Assets/CosmicTreasure_Root/Scripts/PickUps/Camera_Disable.cs
@@ -23,8 +23,8 @@ public class Camera_Disable : MonoBehaviour
         if (controller.isInRange)
         {
             //La camara deja de moverse y no puede detectar al jugador
-            //AudioManager.instance.PlaySFX(21);
-            //AudioManager.instance.PlaySFX(1);
+            AudioManager.instance.PlaySFX(21);
+            AudioManager.instance.PlaySFX(1);
             //fov.range = 0.1f;
             //fov.rotationSpeed = 0.0f;
             controller.isClicked = true;
