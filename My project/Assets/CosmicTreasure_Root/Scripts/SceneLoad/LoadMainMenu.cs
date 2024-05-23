34f2d155f2a9179de521be04109711d1a66fbfb2
mote44 <137623605+mote44@users.noreply.github.com>
2024-05-23 11:44:27 +0200

    gfs

--- My project/Assets/CosmicTreasure_Root/Scripts/SceneLoad/LoadMainMenu.cs
+++ My project/Assets/CosmicTreasure_Root/Scripts/SceneLoad/LoadMainMenu.cs
@@ -2,16 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LoadMainMenu : MonoBehaviour
 {
+    public GameObject optPanel;
     private void Start()
     {
         //MainMenu();
+
     }
 
     public void MainMenu()
     {
         SceneManager.LoadScene("Main Menu");
+    }
+
+    public void ResumePlay()
+    {
+        optPanel.SetActive(false);
     }
 }
